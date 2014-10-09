3258bd23ff9e352c22e647a2f02f9a27c79c1469
Brandon <bwood1@ggc.edu>
2014-10-08 23:25:40 -0400

    Initial commit

---
+++ application/views/home.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Naviweb Home</title>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('styles/bootstrap.min.css'); ?>"></style>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('styles/style.css'); ?>"></style>
+        <script type="text/javascript" src="<?php echo base_url('js/jquery.js'); ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url('js/bootstrap.min.js'); ?>"></script>
+</head>
+<body>
+    <div class="container-fluid">
+        
+        <nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Naviweb</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">File</a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Edit</span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+        
+        
+        <!---->
+        <!--<div class="form-group">-->
+        <!--    <div class="row">-->
+        <!--        <div>-->
+        <!--            <button class="btn">-->
+        <!--                File-->
+        <!--            </button>-->
+        <!--        </div>-->
+        <!--        <div>-->
+        <!--            <button class="btn">-->
+        <!--                Edit-->
+        <!--            </button>-->
+        <!--        </div>-->
+        <!--        <div>-->
+        <!--            <button class="btn">-->
+        <!--                Menu Item-->
+        <!--            </button>-->
+        <!--        </div>-->
+        <!--        <div>-->
+        <!--            <button class="btn">-->
+        <!--                Menu Item-->
+        <!--            </button>-->
+        <!--        </div>-->
+        <!--        <div>-->
+        <!--            <button class="btn">-->
+        <!--                Menu Item-->
+        <!--            </button>-->
+        <!--        </div>-->
+        <!--        <div>-->
+        <!--            <button class="btn">-->
+        <!--                Menu Item-->
+        <!--            </button>-->
+        <!--            -->
+        <!--        </div>-->
+                
+                <!--<div class="col-md-1 col-lg-1">-->
+                <!--    -->
+                <!--</div>-->
+                <!--<div class="col-md-1 col-lg-1">-->
+                <!--    Edit-->
+                <!--</div>-->
+                <!--<div class="col-md-1 col-lg-1">-->
+                <!--    Menu Item-->
+                <!--</div>-->
+                <!--<div class="col-md-1 col-lg-1">-->
+                <!--    Menu Item 2-->
+                <!--</div>-->
+                <!--<div class="col-md-1 col-lg-1">-->
+                <!--    Menu Item 3-->
+                <!--</div>-->
+                <!--<div class="col-md-1 col-lg-1">-->
+                <!--    Menu Item 4-->
+                <!--</div>-->
+            <!--</div>-->
+            <!--<div class="row">-->
+            <!--    <div class="col-md-1">-->
+            <!--        -->
+            <!--    </div>-->
+            <!--</div>-->
+        </div>
+    </div>
+
+</body>
+</html>
